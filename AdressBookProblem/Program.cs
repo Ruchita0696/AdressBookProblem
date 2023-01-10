315472c802f7bd5bdb23c7a8e62b2834f7e81c73
ruchi06 <ruchi06@gmail.com>
2023-01-10 18:10:04 +0530

    Create Contact in Address Book

---
+++ AdressBookProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AdressBookProblem
+{
+    public class Program
+    {
+        public static List<ContactDetails> Person = new List<ContactDetails>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book");
+            AdressBook.CreateContact();
+        }
+    }
+}
