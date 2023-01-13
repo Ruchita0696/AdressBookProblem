c21f35bc46960097fa7ea93bf1e8abec7249d590
ruchi06 <ruchi06@gmail.com>
2023-01-13 17:50:30 +0530

    Edit First Name

--- AdressBookProblem/Program.cs
+++ AdressBookProblem/Program.cs
@@ -9,6 +9,9 @@
             Console.WriteLine("Welcome to Address Book \n");
 
             AdressBook.CreateContact();
+            AdressBook.DisplayContactDetails();
+            AdressBook.EditByFirstName();
+            AdressBook.DisplayContactDetails();
         }
     }
 }
